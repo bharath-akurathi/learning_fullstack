8c754e1395875adfa46463305ffec5df46b82ebb
bharathakurathi436 <139782092+bharath-akurathi@users.noreply.github.com>
2026-01-16 18:55:33 +0530

    Added routes and learnt  about middleware

---
+++ backend/routes/blog.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+// Middleware that is specific to this router
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+})
+
+// define the home page route
+router.get('/', (req, res) => {
+  res.send('Blog home page')
+})
+
+// define the about route
+router.get('/about', (req, res) => {
+  res.send('About blog')
+})
+
+// define the about route
+router.get('/blogpost/:slug', (req, res) => {
+  res.send(`fetch the blogpost for ${req.params.slug}`)
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,73 @@
 const express = require('express')
+const blogs = require('./routes/blog')
+const fs =require('fs')
+
 const app = express()
 const port = 3000
 
+// middleware
+app.use(express.static('public'))
+app.use('/blog',blogs)
+
+// Middleware 1 - Logger for our application
+app.use((req, res, next) => {
+    console.log(req.headers)
+    req.harry = "I am harry bhai";
+    fs.appendFileSync("logs.txt", `${Date.now()} is a ${req.method}\n`)
+    console.log(`${Date.now()} is a ${req.method}`)
+    // res.send("Hacked by Middlware 1")
+    next()
+})
+
+// Middleware 2
+app.use((req, res, next) => {
+    console.log('m2')
+    req.harry = "I am Rohan bhai";
+    next()
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log("Get request");
+    res.send('Hello World. This is from the get request');
+})
+
+app.post('/', (req, res) => {
+    console.log("Post request");
+    res.send('Hello World. This is from the post request');
+})
+
+app.put('/', (req, res) => {
+    console.log("Put request");
+    res.send('Hello World. This is from the put request');
 })
 
 app.get('/about', (req, res) => {
-    res.send('About us')
+    // res.send('About us')
+    res.sendFile('templates/about.html',{root:__dirname})
+})
+
+app.get('/download',(req,res)=>{
+    res.download('templates/about.html',{root:__dirname})
 })
 
 app.get('/contact', (req, res) => {
-    res.send('Hello contact me!')
+    res.send('Hello contact me!'+ req.harry)
 })
 
-app.get('/blog', (req, res) => {
-    res.send('Hello blog!')
-})
+// app.get('/blog', (req, res) => {
+//     res.send('Hello blog!')
+// })
 
-app.get('/blog/:slug', (req, res) => {
-    // logic to fetch {slug} from the db
-    // For URL: http://localhost:3000/blog/intro-to-padosi?mode=dark&region=in
-    console.log(req.params) // will output { slug: 'intro-to-padosi' }
-    console.log(req.query) // will output { mode: 'dark', region: 'in' }
-    res.send(`hello ${req.params.slug}`)
-})
+// app.get('/blog/:slug', (req, res) => {
+//     // logic to fetch {slug} from the db
+//     // For URL: http://localhost:3000/blog/intro-to-padosi?mode=dark&region=in
+//     console.log(req.params) // will output { slug: 'intro-to-padosi' }
+//     console.log(req.query) // will output { mode: 'dark', region: 'in' }
+//     res.send(`hello ${req.params.slug}`)
+// })
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
+    console.log(`Example app listening on http://localhost:${port}`)
 })
 
 
